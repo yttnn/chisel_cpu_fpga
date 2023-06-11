6ba509499e2d552f1b170320cf858a21d65db40f
yttnn <67893139+yttnn@users.noreply.github.com>
2023-06-11 08:48:07 +0000

    decoder update

--- chisel-template/src/main/scala/0_fpga/Core.scala
+++ chisel-template/src/main/scala/0_fpga/Core.scala
@@ -33,35 +33,56 @@ class Core extends Module {
   val imm_s = Cat(inst(31, 25), inst(11, 7))
   val imm_s_sext = Cat(Fill(20, imm_s(11)), imm_s)
 
+  val csignals = ListLookup(inst,
+              List(ALU_X,   OP1_RS1, OP2_RS2, MEN_X,  REN_X, WB_X  ),
+    Array(
+      LW   -> List(ALU_ADD, OP1_RS1, OP2_IMI, MEN_X , REN_S, WB_MEM),
+      SW   -> List(ALU_ADD, OP1_RS1, OP2_IMS, MEN_S,  REN_X, WB_X  ),
+      ADD  -> List(ALU_ADD, OP1_RS1, OP2_RS2, MEN_X , REN_S, WB_ALU),
+      ADDI -> List(ALU_ADD, OP1_RS1, OP2_IMI, MEN_X , REN_S, WB_ALU),
+      SUB  -> List(ALU_SUB, OP1_RS1, OP2_RS2, MEN_X , REN_S, WB_ALU),
+      AND  -> List(ALU_AND, OP1_RS1, OP2_RS2, MEN_X , REN_S, WB_ALU),
+      OR   -> List(ALU_OR , OP1_RS1, OP2_RS2, MEN_X , REN_S, WB_ALU),
+      XOR  -> List(ALU_XOR, OP1_RS1, OP2_RS2, MEN_X , REN_S, WB_ALU),
+      ANDI -> List(ALU_AND, OP1_RS1, OP2_IMI, MEN_X , REN_S, WB_ALU),
+      ORI  -> List(ALU_OR , OP1_RS1, OP2_IMI, MEN_X , REN_S, WB_ALU),
+      XORI -> List(ALU_XOR, OP1_RS1, OP2_IMI, MEN_X , REN_S, WB_ALU)
+    )
+  )
+  val exe_fun :: op1_sel :: op2_sel :: mem_wen :: rf_wen :: wb_sel :: Nil = csignals
+
+  val op1_data = MuxCase(0.U(WORD_LEN.W), Seq(
+    (op1_sel === OP1_RS1) -> rs1_data,
+  ))
+
+  val op2_data = MuxCase(0.U(WORD_LEN.W), Seq(
+    (op2_sel === OP2_RS2) -> rs2_data,
+    (op2_sel === OP2_IMI) -> imm_i_sext,
+    (op2_sel === OP2_IMS) -> imm_s_sext,
+  ))
+
   // ===================
   // Execute
   val alu_out = MuxCase(0.U(WORD_LEN.W), Seq(
-    (inst === LW || inst === ADDI) -> (rs1_data + imm_i_sext),
-    (inst === SW) -> (rs1_data + imm_s_sext),
-    (inst === ADD) -> (rs1_data + rs2_data),
-    (inst === SUB) -> (rs1_data - rs2_data),
-    (inst === AND) -> (rs1_data & rs2_data),
-    (inst === OR) -> (rs1_data | rs2_data),
-    (inst === XOR) -> (rs1_data ^ rs2_data),
-    (inst === ANDI) -> (rs1_data & imm_i_sext),
-    (inst === ORI) -> (rs1_data | imm_i_sext),
-    (inst === XORI) -> (rs1_data ^ imm_i_sext)
+    (exe_fun === ALU_ADD) -> (op1_data + op2_data),
+    (exe_fun === ALU_SUB) -> (op1_data - op2_data),
+    (exe_fun === ALU_AND) -> (op1_data & op2_data),
+    (exe_fun === ALU_OR)  -> (op1_data | op2_data),
+    (exe_fun === ALU_XOR) -> (op1_data ^ op2_data),
   ))
 
   // ===================
   // Memory Access
   io.dmem.addr := alu_out
-  io.dmem.wen := (inst === SW)
+  io.dmem.wen := mem_wen
   io.dmem.wdata := rs2_data
 
   // ===================
   // Write Back
   val wb_data = MuxCase(alu_out, Seq(
-    (inst === LW) -> io.dmem.rdata
+    (wb_sel === WB_MEM) -> io.dmem.rdata
   ))
-  when(inst === LW || inst === ADD || inst === ADDI || inst === SUB
-      || inst === AND || inst === OR || inst === XOR || inst === ANDI
-      || inst === ORI || inst === XORI) {
+  when(rf_wen === REN_S) {
     regfile(wb_addr) := wb_data
   }
   
