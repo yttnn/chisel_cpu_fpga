c21cd93a84b37f47ea2e79899cd7373daca97432
yttnn <67893139+yttnn@users.noreply.github.com>
2023-06-11 14:23:03 +0000

    lui, auipc

--- chisel-template/src/main/scala/0_fpga/Core.scala
+++ chisel-template/src/main/scala/0_fpga/Core.scala
@@ -46,6 +46,8 @@ class Core extends Module {
   val imm_b_sext = Cat(Fill(19, imm_b(11)), imm_b, 0.U(1.U))
   val imm_j = Cat(inst(31), inst(19, 12), inst(20), inst(30, 21))
   val imm_j_sext = Cat(Fill(11, imm_j(19)), imm_j, 0.U(1.U))
+  val imm_u = inst(31,12)
+  val imm_u_shifted = Cat(imm_u, Fill(12, 0.U))
 
   val csignals = ListLookup(inst,
               List(ALU_X,   OP1_RS1, OP2_RS2, MEN_X,  REN_X, WB_X  ),
@@ -79,6 +81,8 @@ class Core extends Module {
       BLTU  -> List(BR_BLTU , OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X  ),
       JAL   -> List(ALU_ADD  , OP1_PC , OP2_IMJ, MEN_X, REN_S, WB_PC ),
       JALR  -> List(ALU_JALR , OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_PC ),
+      LUI   -> List(ALU_ADD  , OP1_X  , OP2_IMU, MEN_X, REN_S, WB_ALU),
+      AUIPC -> List(ALU_ADD  , OP1_PC , OP2_IMU, MEN_X, REN_S, WB_ALU),
     )
   )
   val exe_fun :: op1_sel :: op2_sel :: mem_wen :: rf_wen :: wb_sel :: Nil = csignals
@@ -92,7 +96,8 @@ class Core extends Module {
     (op2_sel === OP2_RS2) -> rs2_data,
     (op2_sel === OP2_IMI) -> imm_i_sext,
     (op2_sel === OP2_IMS) -> imm_s_sext,
-    (op2_sel === OP2_IMJ) -> imm_j_sext
+    (op2_sel === OP2_IMJ) -> imm_j_sext,
+    (op2_sel === OP2_IMU) -> imm_u_shifted
   ))
 
   // ===================
