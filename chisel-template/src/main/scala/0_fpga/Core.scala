dde7da2e61ff470dd4a1bfdba08f497ce0157fdc
yttnn <67893139+yttnn@users.noreply.github.com>
2023-06-11 06:30:25 +0000

    decode

--- chisel-template/src/main/scala/0_fpga/Core.scala
+++ chisel-template/src/main/scala/0_fpga/Core.scala
@@ -19,10 +19,24 @@ class Core extends Module {
   io.imem.addr := pc_reg;
   val inst = io.imem.inst;
 
+  // ==================-
+  // Instruction Decode
+  val rs1_addr = inst(19, 15)
+  val rs2_addr = inst(24, 20)
+  val wb_addr = inst(11, 7)
+  val rs1_data = Mux((rs1_addr =/= 0.U(WORD_LEN.U)), regfile(rs1_addr), 0.U(WORD_LEN.W))
+  val rs2_data = Mux((rs2_addr =/= 0.U(WORD_LEN.U)), regfile(rs2_addr), 0.U(WORD_LEN.W))
+
+
   // ==================
   // Debug
   io.exit := (inst === 0x34333231.U(WORD_LEN.W))
-  printf(p"pc_reg : 0x${Hexadecimal(pc_reg)}\n")
-  printf(p"inst : 0x${Hexadecimal(inst)}\n")
-  printf("---------------\n")
+  printf(p"pc_reg   : 0x${Hexadecimal(pc_reg)}\n")
+  printf(p"inst     : 0x${Hexadecimal(inst)}\n")
+  printf(p"rs1_addr : $rs1_addr\n")
+  printf(p"rs2_addr : $rs2_addr\n")
+  printf(p"wb_addr  : $wb_addr\n")
+  printf(p"rs1_data : 0x${Hexadecimal(rs1_data)}\n")
+  printf(p"rs2_data : 0x${Hexadecimal(rs2_data)}\n")
+  printf("---------\n")
 }
