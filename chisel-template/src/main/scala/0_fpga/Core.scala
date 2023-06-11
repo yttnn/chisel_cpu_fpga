b714a35ce25c10fd48296f72cbf0f2b61b901dd6
yttnn <67893139+yttnn@users.noreply.github.com>
2023-06-11 07:58:36 +0000

    add, addi, sub

--- chisel-template/src/main/scala/0_fpga/Core.scala
+++ chisel-template/src/main/scala/0_fpga/Core.scala
@@ -36,8 +36,10 @@ class Core extends Module {
   // ===================
   // Execute
   val alu_out = MuxCase(0.U(WORD_LEN.W), Seq(
-    (inst === LW) -> (rs1_data + imm_i_sext),
-    (inst === SW) -> (rs1_data + imm_s_sext)
+    (inst === LW || inst === ADDI) -> (rs1_data + imm_i_sext),
+    (inst === SW) -> (rs1_data + imm_s_sext),
+    (inst === ADD) -> (rs1_data + rs2_data),
+    (inst === SUB) -> (rs1_data - rs2_data)
   ))
 
   // ===================
@@ -48,8 +50,10 @@ class Core extends Module {
 
   // ===================
   // Write Back
-  val wb_data = io.dmem.rdata
-  when(inst === LW) {
+  val wb_data = MuxCase(alu_out, Seq(
+    (inst === LW) -> io.dmem.rdata
+  ))
+  when(inst === LW || inst === ADD || inst === ADDI || inst === SUB) {
     regfile(wb_addr) := wb_data
   }
   
