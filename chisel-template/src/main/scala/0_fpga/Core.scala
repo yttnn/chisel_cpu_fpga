e21c80839df59002b5cc5d57d4c67120a24231f6
yttnn <67893139+yttnn@users.noreply.github.com>
2023-06-11 08:07:17 +0000

    and, or, xor, andi, ori, xori

--- chisel-template/src/main/scala/0_fpga/Core.scala
+++ chisel-template/src/main/scala/0_fpga/Core.scala
@@ -39,7 +39,13 @@ class Core extends Module {
     (inst === LW || inst === ADDI) -> (rs1_data + imm_i_sext),
     (inst === SW) -> (rs1_data + imm_s_sext),
     (inst === ADD) -> (rs1_data + rs2_data),
-    (inst === SUB) -> (rs1_data - rs2_data)
+    (inst === SUB) -> (rs1_data - rs2_data),
+    (inst === AND) -> (rs1_data & rs2_data),
+    (inst === OR) -> (rs1_data | rs2_data),
+    (inst === XOR) -> (rs1_data ^ rs2_data),
+    (inst === ANDI) -> (rs1_data & imm_i_sext),
+    (inst === ORI) -> (rs1_data | imm_i_sext),
+    (inst === XORI) -> (rs1_data ^ imm_i_sext)
   ))
 
   // ===================
@@ -53,7 +59,9 @@ class Core extends Module {
   val wb_data = MuxCase(alu_out, Seq(
     (inst === LW) -> io.dmem.rdata
   ))
-  when(inst === LW || inst === ADD || inst === ADDI || inst === SUB) {
+  when(inst === LW || inst === ADD || inst === ADDI || inst === SUB
+      || inst === AND || inst === OR || inst === XOR || inst === ANDI
+      || inst === ORI || inst === XORI) {
     regfile(wb_addr) := wb_data
   }
   
