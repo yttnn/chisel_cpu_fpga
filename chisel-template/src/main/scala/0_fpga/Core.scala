8427b28d7e145c1e47d00e37e6c74ff6c4010fd1
yttnn <67893139+yttnn@users.noreply.github.com>
2023-06-11 13:33:43 +0000

    beq, bne, blt, bge, bltu, bgeu

--- chisel-template/src/main/scala/0_fpga/Core.scala
+++ chisel-template/src/main/scala/0_fpga/Core.scala
@@ -17,9 +17,16 @@ class Core extends Module {
   // ===================
   // Instruction Fetch
   val pc_reg = RegInit(START_ADDR)
-  pc_reg := pc_reg + 4.U(WORD_LEN.W)
-  io.imem.addr := pc_reg;
-  val inst = io.imem.inst;
+  io.imem.addr := pc_reg
+  val inst = io.imem.inst
+  val pc_plus4 = pc_reg + 4.U(WORD_LEN.W)
+  val br_flg = Wire(Bool())
+  val br_target = Wire(UInt(WORD_LEN.W))
+
+  val pc_next = MuxCase(pc_plus4, Seq(
+    br_flg -> br_target
+  ))
+  pc_reg := pc_next
 
   // ==================-
   // Instruction Decode
@@ -32,6 +39,8 @@ class Core extends Module {
   val imm_i_sext = Cat(Fill(20, imm_i(11)), imm_i)
   val imm_s = Cat(inst(31, 25), inst(11, 7))
   val imm_s_sext = Cat(Fill(20, imm_s(11)), imm_s)
+  val imm_b = Cat(inst(31), inst(7), inst(30, 25), inst(11, 8))
+  val imm_b_sext = Cat(Fill(19, imm_b(11)), imm_b, 0.U(1.U))
 
   val csignals = ListLookup(inst,
               List(ALU_X,   OP1_RS1, OP2_RS2, MEN_X,  REN_X, WB_X  ),
@@ -57,6 +66,12 @@ class Core extends Module {
       SLTU  -> List(ALU_SLTU, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
       SLTI  -> List(ALU_SLT , OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
       SLTIU -> List(ALU_SLTU, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+      BEQ   -> List(BR_BEQ  , OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X  ),
+      BNE   -> List(BR_BNE  , OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X  ),
+      BGE   -> List(BR_BGE  , OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X  ),
+      BGEU  -> List(BR_BGEU , OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X  ),
+      BLT   -> List(BR_BLT  , OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X  ),
+      BLTU  -> List(BR_BLTU , OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X  ),
     )
   )
   val exe_fun :: op1_sel :: op2_sel :: mem_wen :: rf_wen :: wb_sel :: Nil = csignals
@@ -85,6 +100,17 @@ class Core extends Module {
     (exe_fun === ALU_SLT) -> (op1_data.asSInt() < op2_data.asSInt()).asUInt(),
     (exe_fun === ALU_SLTU) -> (op1_data < op2_data).asUInt(),
   ))
+  
+  // branch
+  br_flg := MuxCase(false.B, Seq(
+    (exe_fun === BR_BEQ)  ->  (op1_data === op2_data),
+    (exe_fun === BR_BNE)  -> !(op1_data === op2_data),
+    (exe_fun === BR_BLT)  ->  (op1_data.asSInt() < op2_data.asSInt()),
+    (exe_fun === BR_BGE)  -> !(op1_data.asSInt() < op2_data.asSInt()),
+    (exe_fun === BR_BLTU) ->  (op1_data < op2_data),
+    (exe_fun === BR_BGEU) -> !(op1_data < op2_data)
+  ))
+  br_target :=pc_reg + imm_b_sext
 
   // ===================
   // Memory Access
