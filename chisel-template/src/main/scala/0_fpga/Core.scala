b42e6df9d66ffd78d5a6288c2df40eaa53051e12
yttnn <67893139+yttnn@users.noreply.github.com>
2023-06-11 09:11:17 +0000

    sll, srl, sra, slli, srli, srai

--- chisel-template/src/main/scala/0_fpga/Core.scala
+++ chisel-template/src/main/scala/0_fpga/Core.scala
@@ -46,7 +46,13 @@ class Core extends Module {
       XOR  -> List(ALU_XOR, OP1_RS1, OP2_RS2, MEN_X , REN_S, WB_ALU),
       ANDI -> List(ALU_AND, OP1_RS1, OP2_IMI, MEN_X , REN_S, WB_ALU),
       ORI  -> List(ALU_OR , OP1_RS1, OP2_IMI, MEN_X , REN_S, WB_ALU),
-      XORI -> List(ALU_XOR, OP1_RS1, OP2_IMI, MEN_X , REN_S, WB_ALU)
+      XORI -> List(ALU_XOR, OP1_RS1, OP2_IMI, MEN_X , REN_S, WB_ALU),
+      SLL  -> List(ALU_SLL, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+      SRL  -> List(ALU_SRL, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+      SRA  -> List(ALU_SRA, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+      SLLI -> List(ALU_SLL, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+      SRLI -> List(ALU_SRL, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+      SRAI -> List(ALU_SRA, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
     )
   )
   val exe_fun :: op1_sel :: op2_sel :: mem_wen :: rf_wen :: wb_sel :: Nil = csignals
@@ -69,6 +75,9 @@ class Core extends Module {
     (exe_fun === ALU_AND) -> (op1_data & op2_data),
     (exe_fun === ALU_OR)  -> (op1_data | op2_data),
     (exe_fun === ALU_XOR) -> (op1_data ^ op2_data),
+    (exe_fun === ALU_SLL) -> (op1_data << op2_data(4, 0))(31, 0),
+    (exe_fun === ALU_SRL) -> (op1_data >> op2_data(4, 0)).asUInt(),
+    (exe_fun === ALU_SRA) -> (op1_data.asUInt() >> op2_data(4, 0)).asUInt()
   ))
 
   // ===================
