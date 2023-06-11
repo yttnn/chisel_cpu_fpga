84975a41c69afa296281de549371362a41448900
yttnn <67893139+yttnn@users.noreply.github.com>
2023-06-11 14:12:17 +0000

    jal, jalr

--- chisel-template/src/main/scala/0_fpga/Core.scala
+++ chisel-template/src/main/scala/0_fpga/Core.scala
@@ -22,9 +22,12 @@ class Core extends Module {
   val pc_plus4 = pc_reg + 4.U(WORD_LEN.W)
   val br_flg = Wire(Bool())
   val br_target = Wire(UInt(WORD_LEN.W))
+  val jmp_flg = (inst === JAL || inst === JALR)
+  val alu_out = Wire(UInt(WORD_LEN.W))
 
   val pc_next = MuxCase(pc_plus4, Seq(
-    br_flg -> br_target
+    br_flg -> br_target,
+    jmp_flg -> alu_out,
   ))
   pc_reg := pc_next
 
@@ -41,6 +44,8 @@ class Core extends Module {
   val imm_s_sext = Cat(Fill(20, imm_s(11)), imm_s)
   val imm_b = Cat(inst(31), inst(7), inst(30, 25), inst(11, 8))
   val imm_b_sext = Cat(Fill(19, imm_b(11)), imm_b, 0.U(1.U))
+  val imm_j = Cat(inst(31), inst(19, 12), inst(20), inst(30, 21))
+  val imm_j_sext = Cat(Fill(11, imm_j(19)), imm_j, 0.U(1.U))
 
   val csignals = ListLookup(inst,
               List(ALU_X,   OP1_RS1, OP2_RS2, MEN_X,  REN_X, WB_X  ),
@@ -72,23 +77,27 @@ class Core extends Module {
       BGEU  -> List(BR_BGEU , OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X  ),
       BLT   -> List(BR_BLT  , OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X  ),
       BLTU  -> List(BR_BLTU , OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X  ),
+      JAL   -> List(ALU_ADD  , OP1_PC , OP2_IMJ, MEN_X, REN_S, WB_PC ),
+      JALR  -> List(ALU_JALR , OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_PC ),
     )
   )
   val exe_fun :: op1_sel :: op2_sel :: mem_wen :: rf_wen :: wb_sel :: Nil = csignals
 
   val op1_data = MuxCase(0.U(WORD_LEN.W), Seq(
     (op1_sel === OP1_RS1) -> rs1_data,
+    (op1_sel === OP1_PC) -> pc_reg
   ))
 
   val op2_data = MuxCase(0.U(WORD_LEN.W), Seq(
     (op2_sel === OP2_RS2) -> rs2_data,
     (op2_sel === OP2_IMI) -> imm_i_sext,
     (op2_sel === OP2_IMS) -> imm_s_sext,
+    (op2_sel === OP2_IMJ) -> imm_j_sext
   ))
 
   // ===================
   // Execute
-  val alu_out = MuxCase(0.U(WORD_LEN.W), Seq(
+  alu_out := MuxCase(0.U(WORD_LEN.W), Seq(
     (exe_fun === ALU_ADD) -> (op1_data + op2_data),
     (exe_fun === ALU_SUB) -> (op1_data - op2_data),
     (exe_fun === ALU_AND) -> (op1_data & op2_data),
@@ -99,6 +108,7 @@ class Core extends Module {
     (exe_fun === ALU_SRA) -> (op1_data.asSInt() >> op2_data(4, 0)).asUInt(),
     (exe_fun === ALU_SLT) -> (op1_data.asSInt() < op2_data.asSInt()).asUInt(),
     (exe_fun === ALU_SLTU) -> (op1_data < op2_data).asUInt(),
+    (exe_fun === ALU_JALR) -> ((op1_data + op2_data) & ~1.U(WORD_LEN.W)),
   ))
   
   // branch
@@ -115,13 +125,14 @@ class Core extends Module {
   // ===================
   // Memory Access
   io.dmem.addr := alu_out
-  io.dmem.wen := mem_wen
+  io.dmem.wen := Mux(mem_wen === MEN_S, 1.U(MEN_LEN.W), 0.U(MEN_LEN.W))
   io.dmem.wdata := rs2_data
 
   // ===================
   // Write Back
   val wb_data = MuxCase(alu_out, Seq(
-    (wb_sel === WB_MEM) -> io.dmem.rdata
+    (wb_sel === WB_MEM) -> io.dmem.rdata,
+    (wb_sel === WB_PC)  -> pc_plus4
   ))
   when(rf_wen === REN_S) {
     regfile(wb_addr) := wb_data
